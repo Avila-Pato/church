692cac1d8eebbe8d72729ebf8e1b1af0f39fd422
Avila-Pato <p.avilaf1998@gmail.com>
2025-02-21 01:41:46 -0300

    Fix layout classes in HomeView component for improved responsiveness

--- src/app/modules/home/view/home-view.tsx
+++ src/app/modules/home/view/home-view.tsx
@@ -1,11 +1,11 @@
 
 const HomeView = () => {
   return (
-    <div className="w-full h-screen bg-hero bg-cover bg-centeer flex  justify-between items-center ">
+    <div className="h-screen bg-hero bg-cover bg-center flex  justify-between items-center ">
       <div className="w-full ">
         <h1 className="bg-red-700">Hola</h1>
       </div>
-      <div className="flex w-full w-md-1/2 bg-green-400">
+      <div className="flex w-full md:w-1/2 bg-green-400">
               <h2>Periso</h2>
         </div>
     </div>
