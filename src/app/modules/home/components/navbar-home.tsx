040a748d151323f62b8006c9788288c02ccfbf24
Avila-Pato <p.avilaf1998@gmail.com>
2025-02-24 17:03:27 -0300

    Update VSCode settings, refactor navbar for mobile support, and enhance Tailwind configuration

--- src/app/modules/home/components/navbar-home.tsx
+++ src/app/modules/home/components/navbar-home.tsx
@@ -1,15 +1,17 @@
-"use client"
+"use client";
 import Link from "next/link";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
-
+import { Menu, X } from "lucide-react";
 
 export const StudioNavbar = () => {
   const [scrolled, setScrolled] = useState(false);
   const pathName = usePathname();
   const hoverClass = "hover:text-orange-500";
+
+  const [isOpen, setIsOpen] = useState(false);
 
   const links = [
     { name: "Inicio", path: "/" },
@@ -17,64 +19,103 @@ export const StudioNavbar = () => {
     { name: "Ministerio", path: "/ministerio" },
     { name: "Ubicación", path: "/ubicacion" },
     { name: "Calendario", path: "/calendario" },
-  ]
+  ];
 
   useEffect(() => {
     const handleScroll = () => {
       setScrolled(window.scrollY > 50);
-    }
+    };
     window.addEventListener("scroll", handleScroll);
     return () => {
-      window.removeEventListener("scroll", handleScroll);}
-  }, [scrolled]);
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   return (
-    <nav 
-    className={`fixed top-0 left-0 right-0 text-white flex items-center px-2 pr-5 z-50  transition-colors duration-300 ${
-      scrolled ? "bg-gray-800 " : "bg-transparent"
-    }`}
-    
-
+    <nav
+      className={`fixed z-50 top-0 left-0 right-0 text-white flex items-center px-2 pr-5 transition-colors duration-300 ${
+        scrolled ? "bg-gray-800" : "bg-transparent"
+      }`}
     >
-      <div className="flex items-center flex-shrink-0 -my-4">
-        {/* Puedes colocar aqui el sidebar */}
+      {/* Logo */}
+      <div className="flex items-center -my-4">
         <Link href="/">
-          <div className="p-4 flex items-center gap-1 pt-8 ">
+          <div className="p-4 flex items-center gap-1 pt-8">
             <Image
               src="/svg/church.svg"
               alt="Logo-Church"
               width={90}
               height={90}
-              className=" fill-current text-blue-900"
-            ></Image>
+              className="fill-current text-blue-900"
+            />
           </div>
         </Link>
       </div>
-      <div className="flex w-full justify-center items-center">
-      {links.map((link, name) => (
-        <Link href={link.path} key={name}>
-          <div className={` font-normal text-xl  cursor-pointer px-6 ${hoverClass} ${pathName === link.path ? "text-orange-500 font-extrabold" : ""}
-             `}>
-            {link.name}
-          </div>
-        </Link>
-      ) )}
-      </div>
-     
-      <Link href="/">
-        <div className=" flex items-center px-6 ">
-          {/* <Image
-              src="/svg/user.svg"
-              alt="Logo-Church"
-              width={50}
-              height={50}
-            ></Image>
-             */}
-          <Button variant={"default"} className="">
-            Contactanos
-          </Button>
+
+      {/* Botón de menú para móviles y medianas */}
+      <button
+        className="p-4 text-white lg:hidden md:hidden 2xl:hidden"
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        {isOpen ? <X size={24} /> : <Menu size={24} />}
+      </button>
+
+      {/* Menú móvil (se abre desde la izquierda en móviles y medianas) */}
+      <div
+        className={`fixed top-0 left-0 h-full w-64 bg-gray-800 transform transition-transform duration-300 ease-in-out ${
+          isOpen ? "translate-x-0" : "-translate-x-full"
+        } lg:hidden`}
+      >
+        {/* Botón para cerrar el menú móvil */}
+        <button
+          className="absolute top-4 right-4 p-2 text-white"
+          onClick={() => setIsOpen(false)}
+        >
+          <X size={24} />
+        </button>
+
+        <div className="flex flex-col space-y-3 pt-24 pl-6">
+          {links.map((link) => (
+            <Link key={link.name} href={link.path} onClick={() => setIsOpen(false)}>
+              <div
+                className={`font-normal text-xl cursor-pointer ${hoverClass} ${
+                  pathName === link.path ? "text-orange-500 font-extrabold" : ""
+                }`}
+              >
+                {link.name}
+              </div>
+            </Link>
+          ))}
+          {/* Botón de contacto para móviles y medianas */}
+          <Link href="/" onClick={() => setIsOpen(false)}>
+            <Button variant={"default"} className="mt-4 max-w-xs">
+              Contáctanos
+            </Button>
+          </Link>
         </div>
-      </Link>
+      </div>
+
+      {/* Menú de escritorio (visible en pantallas grandes) */}
+      <div className="hidden sm:flex  w-full justify-center items-center">
+        {links.map((link) => (
+          <Link href={link.path} key={link.name}>
+            <div
+              className={`font-normal md:text-lg text-xl cursor-pointer px-6 ${hoverClass} ${
+                pathName === link.path ? "text-orange-500 font-extrabold" : ""
+              }`}
+            >
+              {link.name}
+            </div>
+          </Link>
+        ))}
+      </div>
+
+      {/* Botón de contacto para pantallas grandes */}
+      <div className="hidden sm:flex  items-center px-6">
+        <Link href="/">
+          <Button variant={"default"}>Contáctanos</Button>
+        </Link>
+      </div>
     </nav>
   );
 };
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,45 +1,50 @@
 import type { Config } from "tailwindcss";
 
 export default {
-    darkMode: ["class"],
-    content: [
+  darkMode: ["class"],
+  content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-  	extend: {
-  		colors: {
-  			background: 'var(--background)',
-  			foreground: 'var(--foreground)',
-  			sidebar: {
-  				DEFAULT: 'hsl(var(--sidebar-background))',
-  				foreground: 'hsl(var(--sidebar-foreground))',
-  				primary: 'hsl(var(--sidebar-primary))',
-  				'primary-foreground': 'hsl(var(--sidebar-primary-foreground))',
-  				accent: 'hsl(var(--sidebar-accent))',
-  				'accent-foreground': 'hsl(var(--sidebar-accent-foreground))',
-  				border: 'hsl(var(--sidebar-border))',
-  				ring: 'hsl(var(--sidebar-ring))'
-  			}
-  		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		}
-  	},
-	  backgroundImage: {
-        hero: "url('/img/fondo.jpg')",
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+        sidebar: {
+          DEFAULT: "hsl(var(--sidebar-background))",
+          foreground: "hsl(var(--sidebar-foreground))",
+          primary: "hsl(var(--sidebar-primary))",
+          "primary-foreground": "hsl(var(--sidebar-primary-foreground))",
+          accent: "hsl(var(--sidebar-accent))",
+          "accent-foreground": "hsl(var(--sidebar-accent-foreground))",
+          border: "hsl(var(--sidebar-border))",
+          ring: "hsl(var(--sidebar-ring))",
+        },
       },
-      screens: {
-        xs: '400px',
-        '3xl': '1680px',
-        '4xl': '2200px',
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
       },
-	  fontFamily: {
-        poppins: "var(--font-poppins)", // Usa la variable definida en layout.tsx
-      },
+    },
+    backgroundImage: {
+      hero: "url('/img/fondo.jpg')",
+    },
+    screens: {
+      xs: "400px",
+      sm: "640px", // Pequeño (small)
+      md: "768px", // Mediano (medium)
+      lg: "1024px", // Grande (large) - Añadido aquí
+      xl: "1280px", // Extra grande (extra large)
+      "2xl": "1536px", // Doble extra grande
+      "3xl": "1680px",
+      "4xl": "2200px",
+    },
+    fontFamily: {
+      poppins: "var(--font-poppins)", // Usa la variable definida en layout.tsx
+    },
   },
   plugins: [require("tailwindcss-animate")],
 } satisfies Config;
