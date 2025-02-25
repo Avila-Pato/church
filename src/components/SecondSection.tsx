168bf5414334392695f1bd7d21aeff3a0ddb583b
Avila-Pato <p.avilaf1998@gmail.com>
2025-02-25 01:04:49 -0300

    Refactor SecondSection layout and styling; adjust image visibility and text structure for better responsiveness

--- src/components/SecondSection.tsx
+++ src/components/SecondSection.tsx
@@ -5,26 +5,24 @@ import  useImageToggle  from "../hooks/changeImage";
 const SecondSection = () => {
     const [currentImage, toggleImage] = useImageToggle( "/img/two.jpg", "/img/prayer.jpg");
   return (
-    <section className="flex min-h-screen py-16 ">
-      <figure className=" w-full md:w-1/2 px-6   ">
-        <div className="flex flex-col  ">
+    <section className="flex min-h-screen lg:py-36 ">
+      <figure className=" flex lg:w-1/2 lg:px-6   ">
+        <div className="flex flex-col ">
           <div className="
-           flex flex-col items-start py-12  px-4 z-10  relative text-white left">
+           lg:flex flex-col items-start lg:py-12   lg:px-4 z-10   text-white hidden left">
             <p className="text-2xl tracking-wider">Nuestra Iglesia</p>
             <p  className="text-2xl tracking-wider">Bienvenidos a Imep El Bosque</p>
             </div>
-           
-
           <Image
             src={currentImage}
             alt="church"
             width={900}
             height={100}
-            className="  brightness-75 hover:brightness-100  absolute ml-3 left  "
+            className="  brightness-75 hover:brightness-100  absolute lg:ml-3 left  hidden md:flex "
             />
             </div>
              <div
-              className="bg-opacity-60 p-4 cursor-pointer z-10 items-center  min-h-min flex justify-center  left "
+              className="bg-opacity-60 lg:p-4 cursor-pointer z-10 items-center  min-h-min lg:flex md:hidden justify-center  left "
               onClick={toggleImage}
             >
               <Image
@@ -32,7 +30,7 @@ const SecondSection = () => {
                 alt="next-image"
                 width={300}
                 height={300}
-                className="object-cover rounded-xl object-center transform hover:scale-110 transition duration-300 ease-in-out "
+                className="object-cover rounded-xl object-center transform hover:scale-110 transition duration-300 ease-in-out hidden md:flex "
                 />
                 </div>
        
@@ -40,10 +38,14 @@ const SecondSection = () => {
      
 
        {/* Sección del texto */}
-       <aside className="w-full md:w-1/2 flex flex-col justify-center items-center py-6 px-8 md:px-12 z-10">
-        <div className="max-w-3xl mx-auto  border-2 border-gray-700 border-b-blue-700 border-x-transparent text-white rounded-lg shadow-lg p-8 bottom">
-          <p className="font-poppins text-xl font-bold py-4">
-            <span className="text-2xl text-yellow-400">Bienvenidos</span> a nuestra comunidad cristiana, un espacio donde nos reunimos para fortalecer nuestra fe y crecer espiritualmente.
+       <aside className="lg:w-full lg:md:w-1/2 lg:flex lg:flex-col lg:justify-center lg:items-center py-6 lg:px-8 md:px-12 z-10   ">
+
+          <div className=" flex flex-col   ">         
+        <div className="lg:max-w-3xl lg:mx-auto  border-2 border-gray-700 border-b-blue-700 border-x-transparent
+         text-white rounded-lg shadow-lg lg:p-8 bottom">
+
+          <p className="font-poppins text-md lg:text-xl font-bold py-4 ">
+            <span className=" text-sm lg:text-2xl text-yellow-400">Bienvenidos</span> a nuestra comunidad cristiana, un espacio donde nos reunimos para fortalecer nuestra fe y crecer espiritualmente.
             <br />
             <br />
             Nos reunimos con el propósito de experimentar encuentros con Dios que transformen nuestras vidas, guiándonos a vivir según Su voluntad y reflejar Su amor en todo lo que hacemos.
@@ -54,6 +56,7 @@ const SecondSection = () => {
           <p className="text-lg leading-relaxed">
             Somos una comunidad de fe comprometida a vivir el amor de Dios en todos los aspectos de nuestra vida. Buscamos transformar corazones y mentes a través del encuentro con Dios, guiados por Su propósito y amor.
           </p>
+        </div>
         </div>
       </aside>
 
