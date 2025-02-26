6c3f628dc90e158ecf7344774636d32fd0dcedfc
Avila-Pato <p.avilaf1998@gmail.com>
2025-02-26 13:05:23 -0300

    debuggin

--- src/components/SecondSection.tsx
+++ src/components/SecondSection.tsx
@@ -25,11 +25,11 @@ const SecondSection = () => {
           alt="church"
           width={800}
           height={100}
-          className="  brightness-75 hover:brightness-100  absolute lg:ml-3 left md:flex hidden "
+          className="  brightness-75 hover:brightness-100  absolute lg:ml-3 left  md:hidden hidden lg:flex "
         />
 
         {/* contenedor de la imagen  */}
-        <div className="bg-opacity-60 cursor-pointer z-50 items-center lg:pt-16 lg:w-[300px] lg:flex md:hidden justify-center left lg:flex-shrink-0 ">
+        <div className="bg-opacity-60 cursor-pointer z-50 items-center lg:pt-16 lg:w-[300px] lg:flex md:hidden justify-center left lg:flex-shrink-0  ">
           <Image
             src={
               currentImage === "/img/two.jpg"
@@ -47,7 +47,7 @@ const SecondSection = () => {
       </figure>
 
       {/* Sección del texto */}
-      <aside className="lg:w-full pl-4 lg:md:w-1/2 lg:flex lg:flex-col lg:justify-center lg:items-center py-6 lg:px-8 md:px-12 z-10   ">
+      <aside className="lg:w-full px-2 lg:md:w-1/2 lg:flex lg:flex-col lg:justify-center lg:items-center py-6 lg:px-8 md:px-12 z-10   ">
         <div className=" flex flex-col   ">
           <div
             className="lg:max-w-3xl lg:mx-auto  border-2 border-gray-700 border-b-blue-700 border-x-transparent
@@ -61,35 +61,59 @@ const SecondSection = () => {
               fortalecer nuestra fe y crecer espiritualmente.
               <br />
               <br />
-              <Image
-                src="/img/two.jpg"
-                alt="church"
-                width={200}
-                height={200}
-                className=" flex w-full justify-center  "
-              />
-              <br />
-              Nos reunimos con el propósito de experimentar encuentros con Dios
-              que transformen nuestras vidas, guiándonos a vivir según Su
-              voluntad y reflejar Su amor en todo lo que hacemos.
-              <br />
-              Nuestro llamado es claro: amar a Dios sobre todas las cosas,
-              poniendo a Dios en el centro de nuestra vida diaria.
+              {/* Primer catalogo */}
+              <div className="bg-gray-800  lg:bg-transparent 
+                p-3 lg:p-0 rounded-xl
+                
+                 ">
+                <div className="md:flex  sm:flex justify-center">
+                  <Image
+                    src="/img/two.jpg"
+                    alt="church"
+                    width={200}
+                    height={200}
+                    className="  flex  w-full justify-center
+                        md:w-[900px]
+                        lg:hidden rounded-3xl  
+                "
+                  />
+                  <br />
+                  <div className="text-start md:pt-4 md:pl-8 sm:pl-4  lg:pl-0 lg:pt-0">
+                    Nos reunimos con el propósito de experimentar encuentros con Dios
+                    que transformen nuestras vidas, guiándonos a vivir según Su
+                    voluntad y reflejar Su amor en todo lo que hacemos.
+                    Nuestro llamado es claro: amar a Dios sobre todas las cosas,
+                    poniendo a Dios en el centro de nuestra vida diaria.
+                  </div>
+
+              </div>
+                </div>
               <span>
-              <br />
                 <br />
-                <Image
-                  src="/img/prayer.jpg"
-                  alt="church"
-                  width={200}
-                  height={200}
-                  className=" flex w-full justify-center  "
-                />
                 <br />
-                Somos una comunidad de fe comprometida a vivir el amor de Dios
-                en todos los aspectos de nuestra vida. Buscamos transformar
-                corazones y mentes a través del encuentro con Dios, guiados por
-                Su propósito y amor.
+                {/* Segundo catalogo */}
+                <div className=" bg-gray-800  lg:bg-transparent 
+                p-3 lg:p-0 rounded-xl
+                 ">
+                  <div className="md:flex sm:flex  justify-center">
+                    <Image
+                      src="/img/prayer.jpg"
+                      alt="church"
+                      width={200}
+                      height={200}
+                      className=" flex  w-full justify-center
+                        md:w-[900px]
+                        lg:hidden rounded-3xl  "
+                    />
+                    <br />
+                    <div className="text-start md:pt-8 md:pl-8 sm:pl-4 sm:pt-4  lg:pl-0 lg:pt-0">
+                      Somos una comunidad de fe comprometida a vivir el amor de Dios
+                      en todos los aspectos de nuestra vida. Buscamos transformar
+                      corazones y mentes a través del encuentro con Dios, guiados por
+                      Su propósito y amor.
+                    </div>
+                  </div>
+                </div>
               </span>
             </p>
           </div>
