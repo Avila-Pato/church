8a74c4bd40eb02a588f5d924a62f749bcec60fcf
Avila-Pato <p.avilaf1998@gmail.com>
2025-02-28 01:16:07 -0300

    debuging

--- src/components/SecondSection.tsx
+++ src/components/SecondSection.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Image from "next/image";
 import useImageToggle from "../hooks/changeImage";
-import Link from "next/link";
+
 
 const SecondSection = () => {
   const [currentImage, toggleImage] = useImageToggle(
