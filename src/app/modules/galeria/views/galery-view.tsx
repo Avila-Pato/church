cde9e25d4fe255a046728d3ed9763834cbe94f3a
Avila-Pato <p.avilaf1998@gmail.com>
2025-03-02 16:30:23 -0300

    Setting up clurdinary

--- src/app/modules/galeria/views/galery-view.tsx
+++ src/app/modules/galeria/views/galery-view.tsx
@@ -1,16 +1,16 @@
 import { CategoriesSection } from "../components/gallery-navbar";
+import ImageGallery from "@/components/GalleryImages";
 
 interface GaleryProps {
     categoryId?: string;
+    ImageGallery?: string
   }
 
 const GaleryView = ({categoryId}: GaleryProps ) => {
     return ( 
         <main className="min-h-screen  my-20 ">
             <CategoriesSection  categoryId={categoryId}/>
-            <div className="flex h-[500px] flex-col justify-center items-center   text-white">
-
-                Este es el gallery View</div>
+            <ImageGallery categoryId={categoryId} />
         </main>
      );
 }
---
+++ src/components/GalleryImages.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+interface ImageGalleryProps {
+  categoryId?: string;
+}
+
+const ImageGallery: React.FC<ImageGalleryProps> = ({ categoryId }) => {
+  const images = [
+    "/iglesia/5.jpg",
+    "/iglesia/3.jpg",
+    "/iglesia/4.jpg",
+    "/iglesia/6.jpg",
+    "/iglesia/7.jpg",
+    "/iglesia/1.jpg",
+    "/iglesia/8.jpg",
+    "/iglesia/9.jpg",
+    "/iglesia/10.jpg",
+    "/iglesia/2.jpg",
+  ];
+
+  return (
+    // los estilos para las imagenes se manejan en el global.css
+    <div className="image-gallery">
+      {images.map((src, index) => (
+        <div key={index} className="image-item">
+          <img
+            src={src}
+            alt={`Imagen ${index + 1}` }
+          />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default ImageGallery;
