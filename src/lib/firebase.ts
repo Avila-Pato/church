e804c604e42309d6595cb79799c8943ee9bbabce
Avila-Pato <p.avilaf1998@gmail.com>
2025-03-07 21:54:52 -0300

    Update Firebase configuration to use environment variables and enhance .gitignore for security

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,16 +1,15 @@
-// src/lib/firebase.ts
 import { initializeApp, getApps, cert } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
-import serviceAccount from "../lib/firebase-service-account.json";
 
-// Configura Firebase con las credenciales
+// Configura Firebase con las credenciales desde variables de entorno
 const firebaseConfig = {
   credential: cert({
-    projectId: serviceAccount.project_id,
-    privateKey: serviceAccount.private_key.replace(/\\n/g, "\n"), // Ajusta el formato de la clave privada
-    clientEmail: serviceAccount.client_email,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
   }),
 };
+
 
 // Inicializa Firebase solo si no está ya inicializado
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
