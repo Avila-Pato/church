2773be085b140b71436911c09b9e550ab5d7c89f
Avila-Pato <p.avilaf1998@gmail.com>
2025-04-29 19:18:18 -0400

    Add Content Security Policy headers for enhanced security

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,4 @@
-
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     remotePatterns: [
@@ -15,18 +15,37 @@ const nextConfig = {
         headers: [
           {
             key: "Content-Security-Policy",
+
             value: `
-              default-src 'self';
-              script-src 'self' 'unsafe-inline' 'unsafe-eval' https://upload-widget.cloudinary.com;
-              style-src 'self' 'unsafe-inline';
-              img-src 'self' https://res.cloudinary.com data:;
-              connect-src 'self' https://api.cloudinary.com;
-              frame-src https://upload-widget.cloudinary.com;
-            `.replace(/\n/g, ' ').trim()
-          }
-        ]
-      }
-    ]
+            default-src 'self';
+            script-src
+              'self'
+              'unsafe-inline'
+              'unsafe-eval'
+              https://upload-widget.cloudinary.com
+              https://apis.google.com
+              https://accounts.google.com;
+            style-src 'self' 'unsafe-inline';
+            img-src 'self' https://res.cloudinary.com data:;
+            connect-src
+              'self'
+              https://api.cloudinary.com
+              https://securetoken.googleapis.com
+              https://identitytoolkit.googleapis.com
+              https://www.googleapis.com;
+              frame-src
+                'self'
+                https://upload-widget.cloudinary.com
+                https://church2-40181.firebaseapp.com;
+          `.replace(/\n/g, " ").trim(),
+          },
+          {
+            key: "Cross-Origin-Opener-Policy",
+            value: "same-origin-allow-popups",
+          },
+        ],
+      },
+    ];
   },
   webpack: (config: { resolve: { extensions: string[]; alias: { [x: string]: string; }; }; }) => {
     config.resolve.extensions.push(".json");
